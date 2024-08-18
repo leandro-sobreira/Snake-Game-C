73ee625b44bc4c9d4b2ead8d76007c2319a5a551
leandro-sobreira <lperes16623@gmail.com>
2024-08-18 13:27:23 -0400

    Move and apple

--- Snake.c
+++ Snake.c
@@ -1,6 +1,6 @@
 #include <SDL2/SDL.h>
-#include <time.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 /*
 ====================== SNAKE GAME ======================
@@ -18,35 +18,30 @@ g++ .\Snake.c -I ..\SDL2\x86_64-w64-mingw32\include\ -L ..\SDL2\x86_64-w64-mingw
 #define windowW 640
 #define windowH 480
 
-#define cellsNum 20  // Number of cells in the grid (cellNum*cellNum)
+#define cellsNum 20  // Number of cells in the grid = (cellNum*cellNum)
 #define gridSize 400 //  Size of grid (in pixels)
+#define cellSize gridSize/cellsNum
 
 #define centerGridH ((windowH - gridSize) / 2) - (gridSize / cellsNum)
 #define centerGridW ((windowW - gridSize) / 2) - (gridSize / cellsNum)
 
+
+typedef struct
+{
+    int x;
+    int y;
+} tObj;
+
 typedef struct
 {
     int size;
-    int x[(cellsNum * cellsNum) + 1];
-    int y[(cellsNum * cellsNum) + 1];
+    tObj pos[(cellsNum*cellsNum)+1];
     int moveDir; // 1 = UP, 2 = RIGHT, 3 = DOWN, 4 = LEFT
-    int moved;
+    bool moved;
     int score;
     SDL_Color hRGB;
     SDL_Color tRGB;
-} Snake;
-
-Snake player1 =
-    {
-        1,   // start size
-        {2}, // start X
-        {2}, // start Y
-        2,   // start movedir
-        false,
-        0,
-        {0, 180, 0}, // head Color
-        {0, 255, 0}  // tail Color
-};
+} tSnake;
 
 int main(int argc, char **argv)
 {
@@ -60,53 +55,126 @@ int main(int argc, char **argv)
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
     SDL_Rect cell;
-    cell.h = gridSize / cellsNum;
-    cell.w = gridSize / cellsNum;
+    cell.h = cellSize;
+    cell.w = cellSize;
+
+    tSnake player1 =
+    {
+        1,   // start size
+        {{2, 2}, {1,1}}, // start X Y
+        2,   // start movedir
+        false,
+        0,
+        {0, 180, 0}, // head Color
+        {0, 255, 0}  // tail Color
+    };
+
+    tObj apple = {rand()%cellsNum, rand()%cellsNum};    
 
     while (true)
     {
         SDL_RenderClear(renderer); // Clear the Screen
+        
 
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
-            while (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE) // Verify if the user close the game or press ESC
+            if(event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE) // Verify if the user close the game or press ESC
             {
                 exit(0); // Stop the program
             }
+            if (event.type == SDL_KEYDOWN)
+            {
+                if (player1.moved)
+                {
+                    if (event.key.keysym.sym == SDLK_w && player1.moveDir != 3 && player1.moveDir != 1)
+                    {
+                        player1.moveDir = 1;
+                        player1.moved = false;
+                    } else if (event.key.keysym.sym == SDLK_d && player1.moveDir != 4 && player1.moveDir != 2)
+                    {
+                        player1.moveDir = 2;
+                        player1.moved = false;
+                    } else if (event.key.keysym.sym == SDLK_s && player1.moveDir != 1 && player1.moveDir != 3)
+                    {
+                        player1.moveDir = 3;
+                        player1.moved = false;
+                    } else if (event.key.keysym.sym == SDLK_a && player1.moveDir != 2 && player1.moveDir != 4)
+                    {
+                        player1.moveDir = 4;
+                        player1.moved = false;
+                    }
+                }                
+                
+            }
+            
         }
 
-        // Objects
+        //Move Player
+        switch (player1.moveDir)
+        {
+            case 1:
+                player1.pos[0].y += -1;
+                player1.moved = true;
+            break;
+
+            case 2:
+                player1.pos[0].x += 1;
+                player1.moved = true;
+            break;
+
+            case 3:
+                player1.pos[0].y += 1;
+                player1.moved = true;
+            break;
+
+            case 4:
+                player1.pos[0].x += -1;
+                player1.moved = true;
+            break;
+        }     
+           
+         //Eat apple
+        if (player1.pos[0].x == apple.x && player1.pos[0].y == apple.y)
+        {
+            apple = {rand()%cellsNum, rand()%cellsNum}; 
+        }
+
+        // Render Objects
         for (int i = 1; i < cellsNum; i++)
         {
+            cell.x = centerGridW + i*cellSize;
             for (int j = 1; j < cellsNum; j++)
             {
-                cell.x = centerGridW + i*(gridSize / cellsNum);
-                cell.y = centerGridH + j*(gridSize / cellsNum);
-
+                cell.y = centerGridH + j*cellSize;
 
                 // render Cells
-                if ((i+j) % 2 == 0)
+                if ((i+j) % 2 == 0) // Chess patern
                 {
-                    SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255); // Defaut color
+                    SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
                 } else {
-                    SDL_SetRenderDrawColor(renderer, 40, 40, 40, 255); // Defaut color
+                    SDL_SetRenderDrawColor(renderer, 40, 40, 40, 255);
                 }
 
+                //Render Apple
+                if (apple.x == i && apple.y == j)
+                {
+                    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                } 
+
                 // Render Player
-                if (i == player1.x && j == player1.y){
-                    SDL_SetRenderDrawColor(renderer, player1.hRGB, 255); // Defaut color
+                    if (i == player1.pos[0].x && j == player1.pos[0].y){
+                        SDL_SetRenderDrawColor(renderer, player1.hRGB.r, player1.hRGB.g, player1.hRGB.b, 255);
+                    }
                     
-                }
-                
                 
                 SDL_RenderFillRect(renderer, &cell);               // Fill the Cells
             }
             
         }
+
+       
         
-
-
 
         // BACKGROUND
         SDL_SetRenderDrawColor(renderer, 15, 15, 15, 255); // Background Collor
@@ -114,6 +182,7 @@ int main(int argc, char **argv)
 
         SDL_Delay(100);
     }
+
 
     // END OF PROGRAM
     SDL_DestroyRenderer(renderer);
